32c3cb07f0ded8d71d30a2b32483bff5d30c181e
fulleni <fulleni@hotmail.com>
2025-03-30 05:36:58 +0100

    feat: initial commit for ht_sources_client
    - Added basic library structure
    - Created example class
    - Included tests and analysis options

---
+++ lib/src/ht_sources_client.dart
@@ -0,0 +1,7 @@
+/// {@template ht_sources_client}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class HtSourcesClient {
+  /// {@macro ht_sources_client}
+  const HtSourcesClient();
+}
---
+++ test/src/ht_sources_client_test.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:ht_sources_client/ht_sources_client.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('HtSourcesClient', () {
+    test('can be instantiated', () {
+      expect(HtSourcesClient(), isNotNull);
+    });
+  });
+}
